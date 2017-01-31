0c203e70cd929bdb085bbe8fdbfdd10e33d07852
Thiago Delmotte <thiago.delmotte@gmail.com>
2017-01-30 23:00:50 -0200

    initial project

---
+++ GithubJavaPop/Controllers/AppDelegate/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  GithubJavaPop
+//
+//  Created by Thiago Delmotte on 30/01/17.
+//  Copyright © 2017 Thiago Delmotte. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+  func applicationWillResignActive(_ application: UIApplication) {
+  }
+
+  func applicationDidEnterBackground(_ application: UIApplication) {
+  }
+
+  func applicationWillEnterForeground(_ application: UIApplication) {
+  }
+
+  func applicationDidBecomeActive(_ application: UIApplication) {
+  }
+
+  func applicationWillTerminate(_ application: UIApplication) {
+  }
+
+}
+
