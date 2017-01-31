86cfafeee33fc7bdf53934b2af8e38267a05d6f3
Thiago Delmotte <thiago.delmotte@gmail.com>
2017-01-31 01:22:17 -0200

    navigation / launch screen / main storyboard

--- GithubJavaPop/Classes/Utils/StyleGuide.swift
+++ GithubJavaPop/Classes/Utils/StyleGuide.swift
@@ -12,6 +12,9 @@ class StyleGuide {
     case White
     case Black
     case Red
+    case Orange
+    case Blue
+    case Gray
     case Transparent
     
     var value: UIColor {
@@ -19,9 +22,15 @@ class StyleGuide {
         case .White:
           return UIColor(r: 255, g: 255, b: 255, a: 1)
         case .Black:
-          return UIColor(r: 0, g: 0, b: 0, a: 1)
+          return UIColor(r: 52, g: 52, b: 56, a: 1)
         case .Red:
           return UIColor(r: 227, g: 117, b: 126, a: 1)
+        case .Orange:
+          return UIColor(r: 235, g: 164, b: 83, a: 1)
+        case .Blue:
+          return UIColor(r: 72, g: 138, b: 178, a: 1)
+        case .Gray:
+          return UIColor(r: 170, g: 170, b: 171, a: 1)
         case .Transparent:
           return UIColor.clear
       }
