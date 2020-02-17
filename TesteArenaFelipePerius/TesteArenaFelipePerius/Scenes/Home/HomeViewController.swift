1fdaa2565452525bd26fd1b004610c7353216076
felipe <felipe.perius@gmail.com>
2020-02-16 21:00:08 -0300

    add clean architeture struct and setup navigation controller

---
+++ TesteArenaFelipePerius/TesteArenaFelipePerius/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  TesteArenaFelipePerius
+//
+//  Created by Felipe perius on 16/02/20.
+//  Copyright © 2020 Felipe Perius. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBarItems()
+    }
+}
