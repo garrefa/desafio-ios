0f4fc1bafe3d1927c5ae66996ced01d1d24bac83
felipe <felipe.perius@gmail.com>
2020-02-16 23:02:50 -0300

    implementation auto layout in cell

---
+++ TesteArenaFelipePerius/TesteArenaFelipePerius/View/Cells/RepositoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoryTableViewCell.swift
+//  TesteArenaFelipePerius
+//
+//  Created by Felipe perius on 16/02/20.
+//  Copyright © 2020 Felipe Perius. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+
+    static let identifier: String = "RepositoryTableViewCell"
+    
+    static var cellHeight: CGFloat {
+        return 140
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
