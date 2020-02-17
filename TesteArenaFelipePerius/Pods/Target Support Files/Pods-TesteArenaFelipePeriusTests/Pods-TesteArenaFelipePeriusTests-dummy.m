223a5b77554def97c9160ef9f515e68ba66c857b
felipe <felipe.perius@gmail.com>
2020-02-17 13:32:13 -0300

    install cocoapods

---
+++ TesteArenaFelipePerius/Pods/Target Support Files/Pods-TesteArenaFelipePeriusTests/Pods-TesteArenaFelipePeriusTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TesteArenaFelipePeriusTests : NSObject
+@end
+@implementation PodsDummy_Pods_TesteArenaFelipePeriusTests
+@end
--- TesteArenaFelipePerius/TesteArenaFelipePerius/Scenes/Home/HomeViewController.swift
+++ TesteArenaFelipePerius/TesteArenaFelipePerius/Scenes/Home/HomeViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBarItems()
     }
+    
 }
